70b9bc2e400354e55260c205485e2b8b81d06b74
ushasree210 <79778957+ushasree210@users.noreply.github.com>
2023-07-02 22:40:08 -0500

    Updated Program.cs by adding sony

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace Hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");    
-         Console.WriteLine("Hello sony");  }
+         Console.WriteLine("Hello sony");
+              Console.WriteLine("Hello sony darling");
+        }
     }
 }
